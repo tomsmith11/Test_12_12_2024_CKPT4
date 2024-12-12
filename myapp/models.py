b51b1a9c1f44266ac77254a3c317aacebbb03842
Tom Smith <tom@craftscale.uk>
2024-12-12 11:30:01 +0000

    Create teacher and student model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+"""
+The models we have, student and teacher, should be a many to many relationship.
+This means that a student can have many teachers and a teacher can have many students.
+"""
+
+
+"""
+Define Teacher model
+"""
+class Teacher(models.Model):
+  name = models.CharField(max_length=100)
+
+
+"""
+Define Student model
+"""
+class Student(models.Model):
+  name = models.CharField(max_length=100)
